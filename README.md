8658cb199481b853e5b81f7b0e8a134ba6865031
Suraj-Gov <surajgovind332000@gmail.com>
2021-07-30 22:04:54 +0530

    login critic works, need to add password check

---
+++ README.md
@@ -0,0 +1,13 @@
+# Project Docs
+[Project Synopsis](https://docs.google.com/document/d/1fL7SidSWYuzavQPyMzn1btPplhUJ58wV_n5QqON1ZQI/edit?usp=sharing)
+
+# TODO
+
+1. Handle critic signup (DONE)
+2. Handle critic login
+3. CRUD operations on movies
+4. Movie metadata
+5. Add reviews
+6. View movies
+7. Search movie by title, cast or director
+8. Detailed movie view
---
+++ components.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from theme import *
+
+
+def header(root, title):
+    x = Label(root,
+              pady=4,
+              text=title, fg=font_color, bg=bg_color,
+              font=('bold', 32), justify='center')
+    return x
+
+
+def new_window(root, title):
+    x = Toplevel(root)
+    x.geometry(window_geometry)
+    x.resizable(False, False)
+    x.title(title)
+    x.configure(bg=bg_color)
+    return x
+
+
+def button(root, title, command):
+    x = Button(root,
+               text=title, highlightbackground=bg_color, command=command)
+    return x
+
+
+def frame(root):
+    x = Frame(root, padx=4, pady=4, bg=bg_color)
+    return x
+
+
+def centered_frame(root):
+    x = frame(root)
+    x.place(relx=0.5, rely=0.5)
+    return x
+
+
+def form_label(root, title):
+    x = Label(root, text=title, padx=2,
+              bg=bg_color, fg=font_color)
+    return x
+
+
+def input_text(root):
+    x = Entry(
+        root, width=30)
+    return x
